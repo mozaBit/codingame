696cd8a5150bd858548f0b1f7d17be8e5f3b55c9
Bamhammed METEHRI <bmetehri@e1r1p4.clusters.42paris.fr>
2023-06-02 11:04:59 +0200

    Some Algo Updates

--- spring_challenge_2023/main.go
+++ spring_challenge_2023/main.go
@@ -14,16 +14,12 @@ type Map struct {
 }
 
 type Cell struct {
-	myAnts           int
-	oppAnts          int
-	_type            int
-	initialResources int
-	neigh0           int
-	neigh1           int
-	neigh2           int
-	neigh3           int
-	neigh4           int
-	neigh5           int
+	index            int64
+	myAnts           int64
+	oppAnts          int64
+	_type            int64
+	initialResources int64
+	neighbours       [6]int64
 }
 
 type Base struct {
@@ -35,6 +31,8 @@ type Base struct {
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
+var actions []string // actions to do
+var myBase Cell      // my base
 
 func main() {
 	var myMap Map
@@ -58,14 +56,15 @@ func main() {
 		scanner.Scan()
 		fmt.Sscan(scanner.Text(), &_type, &initialResources, &neigh0, &neigh1, &neigh2, &neigh3, &neigh4, &neigh5)
 		var cell Cell
-		cell._type = _type
-		cell.initialResources = initialResources
-		cell.neigh0 = neigh0
-		cell.neigh1 = neigh1
-		cell.neigh2 = neigh2
-		cell.neigh3 = neigh3
-		cell.neigh4 = neigh4
-		cell.neigh5 = neigh5
+		cell.index = int64(i)
+		cell._type = int64(_type)
+		cell.initialResources = int64(initialResources)
+		cell.neighbours[0] = int64(neigh0)
+		cell.neighbours[1] = int64(neigh1)
+		cell.neighbours[2] = int64(neigh2)
+		cell.neighbours[3] = int64(neigh3)
+		cell.neighbours[4] = int64(neigh4)
+		cell.neighbours[5] = int64(neigh5)
 		myMap.cells = append(myMap.cells, cell)
 		if cell.initialResources > 0 {
 			cristalCells = append(cristalCells, strconv.Itoa(i))
@@ -95,9 +94,9 @@ func main() {
 			var resources, myAnts, oppAnts int
 			scanner.Scan()
 			fmt.Sscan(scanner.Text(), &resources, &myAnts, &oppAnts)
-			myMap.cells[i].initialResources = resources
-			myMap.cells[i].myAnts = myAnts
-			myMap.cells[i].oppAnts = oppAnts
+			myMap.cells[i].initialResources = int64(resources)
+			myMap.cells[i].myAnts = int64(myAnts)
+			myMap.cells[i].oppAnts = int64(oppAnts)
 		}
 		/*
 								       ----------- RULES -----------
@@ -119,12 +118,33 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Opp base index: %d\n", myMap.bases[0].oppBaseIndex)
 		fmt.Fprintf(os.Stderr, "Cells with cristals: %s\n", cristalCells)
 		for _, cell := range myMap.cells {
-			fmt.Fprintf(os.Stderr, "Cell: %d, type: %d, resources: %d, neigh0: %d, neigh1: %d, neigh2: %d, neigh3: %d, neigh4: %d, neigh5: %d\n", cell, cell._type, cell.initialResources, cell.neigh0, cell.neigh1, cell.neigh2, cell.neigh3, cell.neigh4, cell.neigh5)
+			fmt.Fprintf(os.Stderr, "Cell: %d, type: %d, resources: %d, neigh0: %d, neigh1: %d, neigh2: %d, neigh3: %d, neigh4: %d, neigh5: %d\n", cell, cell._type, cell.initialResources, cell.neighbours[0], cell.neighbours[1], cell.neighbours[2], cell.neighbours[3], cell.neighbours[4], cell.neighbours[5])
 		}
 
 		// WAIT | LINE <sourceIdx> <targetIdx> <strength> | BEACON <cellIdx> <strength> | MESSAGE <text>
 		// fmt.Println("WAIT")
-		// var actions []string
-		// for _, neighbour := range myMap.bases[0]
+
+		myBase = getBase(myMap, myMap.bases[0].myBaseIndex)
+		actions = make([]string, 0)
+		actions = append(actions, searchPath(myMap, myBase))
 	}
+}
+
+func getBase(myMap Map, BaseIndex int64) Cell {
+	for _, cell := range myMap.cells {
+		if cell.index == BaseIndex {
+			return cell
+		}
+	}
+	return Cell{}
+}
+
+// func to search for the shortest path between cell and my base and return action
+func searchPath(myMap Map, cell Cell) string {
+	for _, neighbour := range cell.neighbours {
+		if getBase(myMap, neighbour).initialResources > 0 {
+			return "BEACON " + strconv.Itoa(int(neighbour)) + " 1"
+		}
+	}
+	return "WAIT"
 }
